6b060230fb2577794a939f94c99f4473475bd69e
guillerp <72713057+guillerp8@users.noreply.github.com>
2021-12-05 16:16:52 +0100

    Add files via upload

---
+++ Shared/Config.lua
@@ -0,0 +1,18 @@
+Config = {} or Config
+
+Config['usingOldESX'] = false
+
+Config['enableNuiIndicator'] = true
+
+Config['scriptVersion'] = 1.0
+
+Config['admingroups'] = {
+    "admin",
+    "superadmin",
+    "mod"
+}
+
+Config["strings"] = {
+    ['open'] = "Press ~b~E~w~ to ~g~open~w~ the door",
+    ['close'] = "Press ~b~E~w~ to ~r~close~w~ the door",
+}
